bee3a2824f884adcd4edb6643a8fff4f6bf1d8fe
neto112 <cristianorneto@gmail.com>
2022-03-07 14:49:44 -0300

    feat: Drag and Drop ordering tasks

--- src/views/Tarefas.vue
+++ src/views/Tarefas.vue
@@ -9,31 +9,24 @@
         @keyup.enter="handleAddTarefa"
       ></v-text-field>
     </v-col>
-    <v-list flat subheader>
-      <v-list-item-group multiple active-class="">
-        <div v-for="(tarefa, index) in $store.state.tarefas" :key="index">
-          <Tarefa :tarefa="tarefa" />
-        </div>
-      </v-list-item-group>
-    </v-list>
+    <ListaDeTarefas />
   </div>
 </template>
 
 <script>
-import Tarefa from "../components/tarefas/Tarefa.vue";
+import ListaDeTarefas from "../components/tarefas/ListaDeTarefas.vue";
 export default {
   name: "Home",
-
   components: {
-    Tarefa,
+    ListaDeTarefas,
   },
   data() {
     return {
       campoInput: null,
     };
   },
-  created(){
-    this.$store.commit('buscaTarefas');
+  created() {
+    this.$store.commit("buscaTarefas");
   },
   methods: {
     handleAddTarefa() {
