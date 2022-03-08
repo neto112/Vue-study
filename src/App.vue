db0763ab9571ee5a76487dea38b0afdd82e3641e
neto112 <cristianorneto@gmail.com>
2022-03-08 09:46:16 -0300

    feat: How to include avatar and background image in the menu

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,22 @@
     <!--  -->
     <!-- </v-navigation-drawer> -->
     <v-navigation-drawer v-model="drawer" app>
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="text-h6"> Vue.js </v-list-item-title>
-          <v-list-item-subtitle> Lista de tarefas </v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
+      <v-img
+        src="https://picsum.photos/1920/1080?random"
+        gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+        dark
+        class="pt-5 text-center"
+      >
+        <v-avatar size="100">
+          <img src="https://avatars.githubusercontent.com/u/79769515?v=4" alt="John" />
+        </v-avatar>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title class="text-h6"> Vue.js </v-list-item-title>
+            <v-list-item-subtitle> Lista de tarefas </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-img>
       <v-divider></v-divider>
       <v-list dense nav>
         <v-list-item v-for="item in items" :key="item.title" :to="item.to" link>
