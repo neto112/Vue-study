acabb8111542e4367364aa390330f2e3cec2c875
neto112 <cristianorneto@gmail.com>
2022-03-17 17:20:13 -0300

    fear: whatsapp

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,86 +1,36 @@
 <template>
-  <div id="app">
-    <v-app>
-      <v-main>
-        <v-carousel>
-          <template v-for="(item, index) in slider">
-            <v-carousel-item
-              v-if="(index + 1) % columns === 1 || columns === 1"
-              :key="index"
-            >
-              <v-row class="flex-nowrap" style="height: 100%">
-                <template v-for="(n, i) in columns">
-                  <template v-if="+index + i < slider.length">
-                    <v-col :key="i">
-                      <v-img
-                        v-if="+index + i < slider.length"
-                        :src="slider[+index + i]"
-                        height="100%"
-                      >
-                        <v-row
-                          class="fill-height"
-                          align="center"
-                          justify="center"
-                        >
-                          <div class="display-3">{{ +index + i + 1 }}</div>
-                        </v-row>
-                      </v-img>
-                    </v-col>
-                  </template>
-                </template>
-              </v-row>
-            </v-carousel-item>
-          </template>
-        </v-carousel>
-      </v-main>
-    </v-app>
-  </div>
+  <a
+  @click="openWhatsApp"
+    class="whatsapp-link"
+    target="_blank"
+  >
+    <v-icon class="mt-1" color="#fff" size="3rem">mdi-whatsapp</v-icon>
+  </a>
 </template>
 
-<script>
+<script lang="ts">
 export default {
-  data(){
-		return {
-      slider: [
-            'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
-            'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
-            'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
-            'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
-            'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
-            'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
-            'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
-            'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
-      ]
-		}
+  methods: {
+    openWhatsApp() {
+      open("https://api.whatsapp.com/send?phone=5511222221111");
+    },
   },
-  computed: {
-    columns() {
-      if (this.$vuetify.breakpoint.xl) {
-        return 4;
-      }
-      if (this.$vuetify.breakpoint.lg) {
-        return 3;
-      }
-      if (this.$vuetify.breakpoint.md) {
-        return 2;
-      }
-      return 1;
-    }
-  },
-}
+};
 </script>
 
-<!-- [
-          {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
-          },
-          {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
-          },
-          {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
-          },
-          {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
-          },
-        ],
+<style>
+.whatsapp-link {
+  position: fixed;
+  width: 60px;
+  height: 60px;
+  bottom: 40px;
+  right: 40px;
+  background-color: #25d366;
+  color: #fff;
+  border-radius: 50%;
+  text-align: center;
+  font-size: 30px;
+  box-shadow: 1px 1px 2px #888;
+  z-index: 1000;
+}
+</style>
